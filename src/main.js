04bd18d44d8661f149003e22a00cf8d319e49e28
Darren Arney <darren.arney@gmail.com>
2025-05-19 15:17:31 -0400

    QoL updates

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ import router from './router' // Add this if you have router
 import ObserveVisibility from 'vue3-observe-visibility'
 import Particles from 'vue3-particles'
 
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  const path = redirect.startsWith('/') ? redirect.substring(1) : redirect;
+  if (path !== '' && path !== '/' && path !== '/index.html') {
+    router.push(path);
+  }
+}
+
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router) // Add this if you have router
