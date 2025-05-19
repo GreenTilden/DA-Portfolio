8debeab3c7f37c8baa3a5817301c4b79b8362fb3
Darren Arney <darren.arney@gmail.com>
2025-05-19 15:22:31 -0400

    ignore

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/DA-Portfolio/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
